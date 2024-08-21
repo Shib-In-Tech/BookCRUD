a35ac227261f525c2a81295d0ac8d48e7885223b
Shibin Spoktech <wespoketech@gmail.com>
2024-08-21 17:32:51 +0530

    seven commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5656
+export const mongoDBURL ="mongodb+srv://wespoketec:wee12234@cluster0.o6kxn.mongodb.net/"
---
+++ backend/middewares/validateBook.js
@@ -0,0 +1,11 @@
+
+const validateBook = (req,res,next)=>{
+    const {title,author,publishYear}= req.body;
+ 
+    if(!title || !author || !publishYear){
+        return res.status(400).send('title,author,publishedYear  are required')
+    }
+    next()
+};
+
+export default validateBook;
